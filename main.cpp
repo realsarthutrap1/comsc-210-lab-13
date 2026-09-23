b4463bb0feb6d4c3e203d503bac027ace54c38a8
sarthucodesagain <261507123+realsarthutrap1@users.noreply.github.com>
2026-09-22 19:32:12 -0700

    lab13: add student struct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 // COMSC-210 | Lab 13 | Sarthak Pani
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
+
+const int MAX_STUDENTS = 200;
+
+struct Student {
+    long id;
+    double score;
+};
+
+int readGrades(Student students[], int maxStudents);
+void selectionSort(Student students[], int count);
+bool writeGrades(const Student students[], int count);
+void printStatistics(const Student students[], int count);
 
 int main() {
     return 0;
